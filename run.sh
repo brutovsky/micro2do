24b9f63809223f66d6810d9e81bf72aae96240d7
brutovsky <vadim.nakitnyak@gmail.com>
2021-12-24 13:37:35 +0200

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mvn clean install
+rc=$?
+if [ $rc -ne 0 ] ; then
+  echo Could not perform mvn clean install, exit code [$rc]; exit $rc
+fi
 cd todo-service || exit
 mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 docker build -t todo-service-image .

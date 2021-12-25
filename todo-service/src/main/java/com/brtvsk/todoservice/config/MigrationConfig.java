52d54c52fa640cf0113d1082eb4f1b8a72f821f5
brutovsky <vadim.nakitnyak@gmail.com>
2021-12-25 16:24:48 +0200

    Set up database migration with mongock

---
+++ todo-service/src/main/java/com/brtvsk/todoservice/config/MigrationConfig.java
@@ -0,0 +1,26 @@
+package com.brtvsk.todoservice.config;
+
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
+import com.github.cloudyrock.spring.v5.MongockSpring5;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
+
+@Configuration
+public class MigrationConfig {
+    @Value("${mongock.change-logs-scan-package}")
+    private String scanPackage;
+
+    @Bean
+    public MongockSpring5.MongockApplicationRunner mongockApplicationRunnerBean(ApplicationContext springContext, MongoTemplate mongoTemplate) {
+        return MongockSpring5.builder()
+                .setDriver(SpringDataMongo3Driver.withDefaultLock(mongoTemplate))
+                .addChangeLogsScanPackages(List.of(scanPackage))
+                .setSpringContext(springContext)
+                .buildApplicationRunner();
+    }
+}

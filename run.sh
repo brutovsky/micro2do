fc66e0a9af8e6c95539f681ce50724892489c0d8
brutovsky <vadim.nakitnyak@gmail.com>
2022-01-10 19:59:37 +0200

    Update docker-compose and add keycloak custom dockerfile

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mvn clean install -DskipIntegrationTests
+mvn clean install -DskipIntegrationTests -DskipTests
 rc=$?
 if [ $rc -ne 0 ] ; then
   echo Could not perform mvn clean install, exit code [$rc]; exit $rc
 fi
+docker build -t custom-keycloak ./keycloak
 cd todo-service || exit
 mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 docker build -t todo-service-image .
